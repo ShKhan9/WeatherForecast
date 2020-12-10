1f3ff64959cdb3d7790c1785ad4e17f22b5b7e3c
Shehata Gamal <shehatagamal120$gmail.com>
2020-12-10 16:23:39 +0200

    Add unit tests

--- WeatherForecastTests/WeatherForecastTests.swift
+++ WeatherForecastTests/WeatherForecastTests.swift
@@ -10,7 +10,9 @@ import XCTest
 @testable import WeatherForecast
 
 class WeatherForecastTests: XCTestCase {
-
+  
+    let vm = MainViewModel()
+     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,9 +21,39 @@ class WeatherForecastTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testLocalFile() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+          
+        vm.getData(fromLocal: true) { items in
+            
+            XCTAssertNotNil(items ,"Invalid local json file")
+            
+        }
+         
+    }
+    
+    func testRemoteCall() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let exp = expectation(description: "Server fetch")
+       
+        vm.getData(fromLocal:false) { items in
+            
+            XCTAssertNotNil(items,"Invalid server response")
+            
+            XCTAssertTrue(items!.count > 0 , "Array must not be empty")
+            
+            exp.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10.0) { error in
+            
+            print(error!)
+            
+        }
+         
     }
 
     func testPerformanceExample() {
